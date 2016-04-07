f307269d4dea87f70dd6b0d1758a9090a466249d
Will <w.e.brereton@gmail.com>
2016-04-07 16:22:34 -0500

    IDIDSTUFF
    Added more UI

---
+++ Assets/GameControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+    public Text[] board; //array to hold text elements that can have their text component changed to reflect moves
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*
+     * ButtonClicked() is called when any of the 9 buttons is clicked, do stuff here to determine if the button text is set
+     */
+    public void ButtonClicked()
+    {
+
+    }
+
+    /*
+     * NewGame() sets all of the text elements to the empty string, new game stuff to go here later 
+     */ 
+    public void NewGame()
+    {
+        for (int intel=0; intel<board.Length; intel++) //loop through board array and set text to ""
+            board[intel].text = "";
+    }
+}
